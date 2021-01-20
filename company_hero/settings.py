49333b62794e28eb87e5ac7d1c18e4967ea7ca0a
Strongreen <strongreen@live.com>
2021-01-20 18:32:34 -0300

    correçao nome do banco

--- company_hero/settings.py
+++ company_hero/settings.py
@@ -24,15 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('8ygwt13#c+5@*&ww6q&qo0kc182abe)sl7%-h)@4hfsd=a=zd*')
-EMAIL_HOST_USER = os.environ.get('strongreen@live.com')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'nameofapp.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'desafiocompanyhero.herokuapp.com']
 
 # Application definition
 
